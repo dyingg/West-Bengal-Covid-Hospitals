dbb1d863d97b333a28a6bbd8531ecaafd19d9c56
Dying <inktechser@gmail.com>
2020-06-26 08:39:41 +0530

    overlays, but unhappy with design

---
+++ src/frontend/src/Components/ReactMap/GoogleMap.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import MapStyle from "./MapStyle.js";
+const LIBRARIES = ["places"];
+const VERSION = "weekly";
+const loadingElement = (
+  <div
+    style={{
+      height: "100%",
+      width: "100%",
+    }}
+  />
+);
+const MAP_CONTAINER_STYLE = {
+  height: "100%",
+  width: "100%",
+  borderRadius: 5,
+};
+
+const onClick = () => {};
+const onZoomChanged = () => {};
+const handleDragStarted = () => {};
+const handleDragEnded = () => {};
+const onBoundsChanged = () => {};
+const onLoad = () => {};
+const styles = MapStyle;
+const options = {
+  styles,
+  gestureHandling: "greedy",
+  maxZoom: 20,
+  minZoom: 3,
+  draggable: true,
+  keyboardShortcuts: false,
+  scrollwheel: true,
+  fullscreenControl: false,
+  streetViewControl: false,
+  mapTypeControl: false,
+};
+
+function Map({ API_KEY, children, center }) {
+  return (
+    <LoadScript
+      googleMapsApiKey={API_KEY}
+      libraries={LIBRARIES}
+      version={VERSION}
+      loadingElement={loadingElement}
+    >
+      <GoogleMap
+        onLoad={onLoad}
+        zoom={14}
+        mapContainerStyle={MAP_CONTAINER_STYLE}
+        onClick={onClick}
+        onZoomChanged={onZoomChanged}
+        onDragStart={handleDragStarted}
+        onDragEnd={handleDragEnded}
+        clickableIcons={false}
+        center={center}
+        onBoundsChanged={onBoundsChanged}
+        options={options}
+      >
+        {children}
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default React.memo(Map);
--- src/frontend/src/Components/ReactMap/Map.js
+++ src/frontend/src/Components/ReactMap/Map.js
@@ -1,54 +1,124 @@
 import React, { useState } from "react";
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-  OverlayView,
-} from "react-google-maps";
 
-import MapStyle from "./MapStyle.js";
-import Coords from "./Coords.js";
+import { LineChart, Line } from "recharts";
 
-import { compose, withProps } from "recompose";
+import AMRI from "./amri.jpg";
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyDRJIw8Yx_TfaA4NWSnDOD75P7UPLu6j-U&v=3.exp&libraries=geometry,drawing",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `100%`, width: `100%` }} />,
-    mapElement: <div style={{ height: `100%`, width: `100` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-)(({ lat, lng }) => {
-  const [hospitalMarkers, updateHospitalMarkers] = useState(Coords);
+import GoogleMap from "./GoogleMap";
+import Coords from "./Coords";
+
+import { OverlayView } from "@react-google-maps/api";
+
+const divStyle = {};
+
+function HospitalOverlay({ lat, lng, name }) {
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 10,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 20,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 33,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 90,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 35,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 37,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 25,
+      amt: 2100,
+    },
+  ];
 
   return (
-    <GoogleMap
-      defaultOptions={{
-        styles: MapStyle,
-        streetViewControl: false,
-        zoomControl: false,
-        fullscreenControl: false,
-        mapTypeControl: false,
-      }}
-      defaultZoom={15}
-      center={{ lat, lng }}
+    <OverlayView
+      position={{ lat, lng }}
+      mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
     >
-      {Coords.map((val, index) => (
-        <Marker
-          title={val.name}
-          key={index}
-          position={{
-            lat: val.lat,
-            lng: val.lng,
-          }}
+      <div className="overlay">
+        <img
+          src={AMRI}
+          height={"100px"}
+          width={"50px"}
+          style={{ borderRadius: "5px", margin: "0 10px" }}
         />
-      ))}
-    </GoogleMap>
+        <div>
+          <h1
+            style={{
+              color: "#FFFFFF",
+              fontSize: "12px",
+            }}
+          >
+            {name}
+          </h1>
+          <LineChart
+            width={100}
+            height={30}
+            data={data}
+            margin={{ top: 15, bottom: 5 }}
+          >
+            <Line
+              type="monotone"
+              dataKey="pv"
+              stroke="#0676ED"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+          <h1
+            style={{
+              color: "#FFFFFF",
+              fontSize: "20px",
+              fontWeight: "700",
+              marginBottom: 0,
+              paddingBottom: 0,
+            }}
+          >
+            {(Math.random() * 100).toFixed(0)}/100
+          </h1>
+        </div>
+      </div>
+    </OverlayView>
   );
-});
+}
 
-export default MyMapComponent;
+function MyMapComponent({ lat, lng }) {
+  return (
+    <GoogleMap
+      API_KEY={"AIzaSyDRJIw8Yx_TfaA4NWSnDOD75P7UPLu6j-U"}
+      center={{ lat, lng }}
+      children={Coords.map((value) => (
+        <HospitalOverlay lat={value.lat} lng={value.lng} name={value.name} />
+      ))}
+    />
+  );
+}
+
+export default React.memo(MyMapComponent);
